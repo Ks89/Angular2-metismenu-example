d7c240791744cf96576e6e843d400b3f28e6f7eb
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-09-05 23:12:54 +0200

    improvements

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -10,12 +10,18 @@ declare var jQuery:any;
 })
 export default class HomeComponent {
 
-  values: Array<string> = ['2','3','4','5'];
+  values: Array<Object> = [
+    {val: 2, arr: [1,2,3,4]},
+    {val: 3, arr: [1,2,3,4,5,6]},
+    {val: 4, arr: [1]},
+    {val: 5, arr: [1,2]}
+  ];
 
   ngAfterViewInit() {
     jQuery("#menu").metisMenu();
   }
 
   constructor() {
+
   }
 }
